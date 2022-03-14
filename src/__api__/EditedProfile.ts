5df46425551544dd5092aeca39ca28aebe4dbc45
Kim LeeJun <hwanam1111@naver.com>
2022-03-14 18:09:26 +0900

    Fix: edit profile interface

---
+++ src/__api__/EditedProfile.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: EditedProfile
+// ====================================================
+
+export interface EditedProfile {
+  __typename: "User";
+  email: string;
+  verified: boolean;
+}
