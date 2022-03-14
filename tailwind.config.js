78e5da0e70e7746bb42f4ee2d536900929410b7e
Kim LeeJun <hwanam1111@naver.com>
2022-03-14 12:48:50 +0900

    Add: tailwind css exnted lime color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        lime: colors.lime,
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
