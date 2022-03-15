3d730d8618afd8bee343056831d6f4554fa555e2
Kim LeeJun <hwanam1111@naver.com>
2022-03-15 14:03:45 +0900

    Add: includes add ts extension

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   client: {
     tagName: "gql",
-    includes: ['./src/**/*.tsx'],
+    includes: ['./src/**/*.{tsx,ts}'],
     service: {
       name: 'uber-eats-backend',
       url: 'http://127.0.0.1:4000/graphql'
