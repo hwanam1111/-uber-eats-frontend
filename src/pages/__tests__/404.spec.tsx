2a7c7b93cf8106c0c579c2ac84f1990af756e6cc
Kim LeeJun <hwanam1111@naver.com>
2022-03-15 18:14:46 +0900

    Fix: directory move components to pages

---
+++ src/pages/__tests__/404.spec.tsx
@@ -0,0 +1,21 @@
+import { render, waitFor } from '@testing-library/react';
+import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PageNotFound from '../404';
+
+describe('<PageNotFound />', () => {
+  it('renders', async () => {
+    render(
+      <HelmetProvider>
+        <Router>
+          <PageNotFound />
+        </Router>
+      </HelmetProvider>,
+    );
+
+    await waitFor(() =>
+      expect(document.title).toBe('Not Found | Uber eats clone'),
+    );
+  });
+});
